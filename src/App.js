b86873429c1ee2aef67c6ba80b08914703ed4b2e
Steve Johnstone <t-sjohnstone@hotels.com>
2021-12-15 16:31:46 +0000

    First commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        Override Tool
+      </header>
+    </div>
+  );
+}
+
+export default App;
